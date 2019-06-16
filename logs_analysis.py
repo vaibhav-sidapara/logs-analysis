39d3009033520d5bb62763a0d4474b2f7ade1b65
Vaibhav Sidapara <vaibhav.sidapara@gmail.com>
2019-06-17 00:35:54 +0800

    Added code and output for Question 2
    Tweaked the output result to show connection and execution status of database.

--- logs_analysis.py
+++ logs_analysis.py
@@ -10,8 +10,10 @@ def select_query(query):
     # Connecting to database
     try:
         connection = psycopg2.connect(database="news")
+        print("\nPostgreSQL connection is open")
         cursor = connection.cursor()
         cursor.execute(query)
+        print("PostgreSQL query execute")
         return cursor.fetchall()
     except (Exception, psycopg2.Error) as error:
         print ("Error while fetching data from PostgreSQL", error)
@@ -33,5 +35,18 @@ def popular_articles():
         print(' ' + str(i[0]) + ' — ' + str(i[1]) + ' views')
 
 
+def popular_authors():
+    results = select_query("SELECT authors.name, count(*) as views "
+                           "FROM articles JOIN authors "
+                           "ON articles.author = authors.id JOIN log "
+                           "ON articles.slug = substring(log.path, 10) "
+                           "WHERE log.status LIKE '200 OK' "
+                           "GROUP BY authors.name ORDER BY views DESC;")
+    print('\nDisplaying the most popular authors of all time:')
+    for i in results:
+        print(' ' + str(i[0]) + ' — ' + str(i[1]) + ' views')
+
+
 if __name__ == '__main__':
     popular_articles()
+    popular_authors()
